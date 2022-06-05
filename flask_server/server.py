76a966aaacad16b709299e01174a387484a0b1fd
Dagart Allison <65997824+dagartga@users.noreply.github.com>
2022-06-05 11:50:29 -0700

    add post method to contact form

--- flask_server/server.py
+++ flask_server/server.py
@@ -9,22 +9,6 @@ def my_home():
 def html_page(page_name):
     return render_template(page_name)
 
-# @app.route('/index.html')
-# def index():
-#     return render_template('index.html')
-#
-# @app.route('/about.html')
-# def about():
-#     return render_template('about.html')
-#
-# @app.route('/works.html')
-# def works():
-#     return render_template('works.html')
-#
-# @app.route('/contact.html')
-# def contact():
-#     return render_template('contact.html')
-#
-# # @app.route('/components.html')
-# # def components():
-# #     return render_template('components.html')
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    return 'form submitted!'
