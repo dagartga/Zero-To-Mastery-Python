0134e6167b83121029e7797384c81de4a182c3bc
Dagart Allison <65997824+dagartga@users.noreply.github.com>
2022-06-05 10:56:35 -0700

    Add downloaded html template

--- flask_server/server.py
+++ flask_server/server.py
@@ -1,19 +1,22 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/<username>/<int:post_id>')
-def hello_world(username=None, post_id=None):
-    return render_template('./index.html', name=username, post_id=post_id)
+@app.route('/')
+def my_home():
+    return render_template('index.html')
 
 @app.route('/about.html')
 def about():
-    return render_template('./about.html')
+    return render_template('about.html')
 
-@app.route('/blog')
-def blog():
-    return 'This is the blog page'
+@app.route('/works.html')
+def works():
+    return render_template('works.html')
 
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
 
-@app.route('/blog/2020')
-def blog2020():
-    return 'This is the blog page for 2020'
+@app.route('/components.html')
+def components():
+    return render_template('components.html')
